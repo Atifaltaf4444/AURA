973872d0e533ab90531d7ff4b24f6c2ca3defb2b
AtifAltaf <atifaltaf4444@gmail.com>
2026-02-13 15:31:58 +0530

    updates

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function openSidebar() {
 function closeSidebar() {
   document.querySelector(".sidebar").style.display = "none";
 }
+
+function showPopup(){
+  document.querySelector(".popup").style.display="block";
+}
+
+function closePopup(){
+  document.querySelector(".popup").style.display="none";
+}
